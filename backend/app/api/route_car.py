48f16840cb008f8f738dd323bb8253540a3fb408
Javier Sánchez <javier.sanchez-castro@epitech.eu>
2025-04-21 01:18:54 +0200

    [UPDATE] route_car

--- backend/app/api/route_car.py
+++ backend/app/api/route_car.py
@@ -1,10 +1,18 @@
 from fastapi import APIRouter
+from fastapi.responses import StreamingResponse
 from api.utils import SessionDB
 from db.daos.CarDAO import CarDAO
-from pydantic import UUID4
+from db.daos.BookingDAO import BookingDAO
+from pydantic import UUID4, FutureDate
 from api.utils import SessionDB
 from db.services.CarService import CarService
-from db.schemas.car import CarShow
+from db.schemas.car import CarShow, CarShow_MaxDays
+from db.schemas.booking import BookingShow
+from datetime import date, timedelta
+from fastapi import Query
+from typing import Annotated
+from pydantic import Field
+from db.schemas.utils import FutureDateOneYear
 
 router = APIRouter()
 
@@ -13,16 +21,23 @@ async def get_all(db: SessionDB):
     cars = CarDAO(db).get_all()
     return cars
 
-@router.get("/available", response_model=list[CarShow])
-async def get_available(db: SessionDB):
-    cars = CarDAO(db).get_available()
-    return cars
 
-@router.get("/unavailable", response_model=list[CarShow])
-async def get_unavailable(db: SessionDB):
-    cars = CarDAO(db).get_unavailable()
-    return cars
+@router.get("/available", response_model=list[CarShow_MaxDays])
+async def get_available(date: FutureDateOneYear, db: SessionDB):
+    return CarService.get_availables(date_selected=date, db=db)
+
+
+@router.get("/available/image", response_model=list[CarShow])
+async def get_available_image(db: SessionDB):
+    image_bytes = CarService.get_availables_image(db)
+    return StreamingResponse(image_bytes, media_type="image/png")
+
 
 @router.get("/{uuid}", response_model=CarShow)
 async def get_by_uuid(uuid: UUID4, db: SessionDB):
     return CarService.get_by_uuid(uuid=uuid, db=db)
+
+
+@router.get("/{uuid}/bookings", response_model=list[BookingShow])
+async def get_car_bookings(uuid: UUID4, db: SessionDB):
+    return CarService.get_by_uuid(uuid=uuid, db=db).bookings_future
