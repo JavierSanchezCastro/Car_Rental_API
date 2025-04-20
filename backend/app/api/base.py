aa3f1ce6c9cac7a24b9686bb330b88ee6e225fc2
Javier Sánchez <javier.sanchez-castro@epitech.eu>
2025-04-21 01:21:36 +0200

    [ADD] booking to main router

--- backend/app/api/base.py
+++ backend/app/api/base.py
@@ -1,6 +1,8 @@
 
 from api import route_car
+from api import route_booking
 from fastapi import APIRouter
 
 api_router = APIRouter()
 api_router.include_router(route_car.router, prefix="/car", tags=["Car"])
+api_router.include_router(route_booking.router, prefix="/booking", tags=["Booking"])
